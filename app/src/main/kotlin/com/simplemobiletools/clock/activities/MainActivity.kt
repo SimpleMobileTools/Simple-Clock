87566f03c7a9d909e7cffbcf51c622890b469151
tibbi <tibor@kaputa.sk>
2018-02-28 00:02:08 +0100

    initial commit

---
+++ app/src/main/kotlin/com/simplemobiletools/clock/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.simplemobiletools.clock.activities
+
+import android.os.Bundle
+import com.simplemobiletools.clock.R
+
+class MainActivity : SimpleActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
