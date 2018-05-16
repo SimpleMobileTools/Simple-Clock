616f6caa15d72dfde9146022d6aa9ba4d174fd58
tibbi <tibor@kaputa.sk>
2018-05-16 10:19:57 +0200

    adding colored launcher icons

--- app/src/main/kotlin/com/simplemobiletools/clock/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/clock/activities/MainActivity.kt
@@ -30,7 +30,7 @@ class MainActivity : SimpleActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        appLaunched()
+        appLaunched(BuildConfig.APPLICATION_ID)
 
         // just get a reference to the database to make sure it is created properly
         dbHelper
