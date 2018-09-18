fbb3fc6b7b3f728d4dc1622195097b381c2b825f
tibbi <tibor@kaputa.sk>
2018-09-18 22:53:15 +0200

    fix #20, make widget text scaling more dynamic

--- app/src/main/kotlin/com/simplemobiletools/clock/helpers/MyWidgetDateTimeProvider.kt
+++ app/src/main/kotlin/com/simplemobiletools/clock/helpers/MyWidgetDateTimeProvider.kt
@@ -18,6 +18,7 @@ import java.util.*
 class MyWidgetDateTimeProvider : AppWidgetProvider() {
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
         performUpdate(context)
         context.scheduleNextWidgetUpdate()
     }
@@ -30,13 +31,9 @@ class MyWidgetDateTimeProvider : AppWidgetProvider() {
     private fun performUpdate(context: Context) {
         val appWidgetManager = AppWidgetManager.getInstance(context)
         appWidgetManager.getAppWidgetIds(getComponentName(context)).forEach {
-            val bundle = appWidgetManager.getAppWidgetOptions(it)
-            val minHeight = bundle.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT)
-            val isSmallLayout = getCellsForSize(minHeight) == 1
-
             val layout = if (context.config.useTextShadow) R.layout.widget_date_time_with_shadow else R.layout.widget_date_time
             RemoteViews(context.packageName, layout).apply {
-                updateTexts(context, this, isSmallLayout)
+                updateTexts(context, this)
                 updateColors(context, this)
                 setupAppOpenIntent(context, this)
                 appWidgetManager.updateAppWidget(it, this)
@@ -44,33 +41,14 @@ class MyWidgetDateTimeProvider : AppWidgetProvider() {
         }
     }
 
-    private fun updateTexts(context: Context, views: RemoteViews, isSmallLayout: Boolean) {
-        val calendar = Calendar.getInstance()
-        val use24HourFormat = context.config.use24HourFormat
-
+    private fun updateTexts(context: Context, views: RemoteViews) {
         val timeText = context.getFormattedTime(getPassedSeconds(), false, false).toString()
-        val dimenResource = if (isSmallLayout) R.dimen.widget_time_text_size_small else R.dimen.widget_time_text_size_big
-        val timeTextSize = context.resources.getDimension(dimenResource) / context.resources.displayMetrics.density
-
-        val topPadding = if (isSmallLayout) 0 else context.resources.getDimension(R.dimen.tiny_margin).toInt()
-        val bottomPadding = context.resources.getDimension(if (isSmallLayout) R.dimen.tiny_margin else R.dimen.medium_margin).toInt()
-
+        val nextAlarm = getFormattedNextAlarm(context)
         views.apply {
-            setViewPadding(R.id.widget_date_time_holder, 0, topPadding, 0, bottomPadding)
-            if (use24HourFormat) {
-                setText(R.id.widget_time, timeText)
-            } else {
-                val timeParts = timeText.split(" ")
-                setText(R.id.widget_time, timeParts[0])
-                setText(R.id.widget_time_am_pm, " ${timeParts[1]}")
-            }
-            setText(R.id.widget_date, context.getFormattedDate(calendar))
-            setTextSize(R.id.widget_time, timeTextSize)
-            setVisibleIf(R.id.widget_time_am_pm, !use24HourFormat)
-
-            val nextAlarm = getFormattedNextAlarm(context)
-            setVisibleIf(R.id.widget_alarm_holder, nextAlarm.isNotEmpty())
+            setText(R.id.widget_time, timeText)
+            setText(R.id.widget_date, context.getFormattedDate(Calendar.getInstance()))
             setText(R.id.widget_next_alarm, nextAlarm)
+            setVisibleIf(R.id.widget_alarm_holder, nextAlarm.isNotEmpty())
         }
     }
 
@@ -82,7 +60,6 @@ class MyWidgetDateTimeProvider : AppWidgetProvider() {
         views.apply {
             setBackgroundColor(R.id.widget_date_time_holder, widgetBgColor)
             setTextColor(R.id.widget_time, widgetTextColor)
-            setTextColor(R.id.widget_time_am_pm, widgetTextColor)
             setTextColor(R.id.widget_date, widgetTextColor)
             setTextColor(R.id.widget_next_alarm, widgetTextColor)
 
@@ -140,8 +117,8 @@ class MyWidgetDateTimeProvider : AppWidgetProvider() {
     }
 
     override fun onAppWidgetOptionsChanged(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int, newOptions: Bundle?) {
-        performUpdate(context)
         super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions)
+        performUpdate(context)
     }
 
     private fun getMultiplyColoredBitmap(resourceId: Int, newColor: Int, context: Context): Bitmap {
@@ -154,13 +131,5 @@ class MyWidgetDateTimeProvider : AppWidgetProvider() {
         val canvas = Canvas(bmp)
         canvas.drawBitmap(bmp, 0f, 0f, paint)
         return bmp
-    }
-
-    private fun getCellsForSize(size: Int): Int {
-        var n = 2
-        while (70 * n - 30 < size) {
-            ++n
-        }
-        return n - 1
     }
 }
