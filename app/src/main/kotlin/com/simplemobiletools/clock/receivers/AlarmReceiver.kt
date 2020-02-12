2ff0d7ee5ed85686a5282e0b79cd71bc5477c6f8
coxtor <coxtor@users.noreply.github.com>
2020-02-12 21:41:22 +0100

    Fix alarm android 10 - 1

--- app/src/main/kotlin/com/simplemobiletools/clock/receivers/AlarmReceiver.kt
+++ app/src/main/kotlin/com/simplemobiletools/clock/receivers/AlarmReceiver.kt
@@ -1,9 +1,15 @@
 package com.simplemobiletools.clock.receivers
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Handler
+import androidx.core.app.NotificationCompat
+import com.simplemobiletools.clock.R
 import com.simplemobiletools.clock.activities.ReminderActivity
 import com.simplemobiletools.clock.extensions.*
 import com.simplemobiletools.clock.helpers.ALARM_ID
@@ -13,17 +19,52 @@ class AlarmReceiver : BroadcastReceiver() {
         val id = intent.getIntExtra(ALARM_ID, -1)
         val alarm = context.dbHelper.getAlarmWithId(id) ?: return
 
-        if (context.isScreenOn()) {
-            context.showAlarmNotification(alarm)
-            Handler().postDelayed({
-                context.hideNotification(id)
-            }, context.config.alarmMaxReminderSecs * 1000L)
-        } else {
-            Intent(context, ReminderActivity::class.java).apply {
-                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                putExtra(ALARM_ID, id)
-                context.startActivity(this)
+            if (context.isScreenOn()) {
+                context.showAlarmNotification(alarm)
+                Handler().postDelayed({
+                    context.hideNotification(id)
+                }, context.config.alarmMaxReminderSecs * 1000L)
+            } else {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+                    val mgr = context.getSystemService(NotificationManager::class.java)
+
+                    if (mgr.getNotificationChannel("Alarm") == null ) {
+                        val channel = NotificationChannel("Alarm","Alarm", NotificationManager.IMPORTANCE_HIGH)
+                        channel.setBypassDnd(true)
+                        mgr.createNotificationChannel(channel)
+
+                    }
+
+                    val pi = PendingIntent.getActivity(
+                            context,
+                            0,
+                            Intent(context, ReminderActivity::class.java).apply{
+                                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                                putExtra(ALARM_ID, id)
+                            },
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    )
+
+                    val builder = NotificationCompat.Builder(context, "Alarm")
+                            .setSmallIcon(R.drawable.ic_alarm_vector)
+                            .setContentTitle("Alarm Active")
+                            .setAutoCancel(true)
+                            .setPriority(NotificationCompat.PRIORITY_HIGH)
+                            .setCategory(NotificationCompat.CATEGORY_ALARM)
+                            .setFullScreenIntent(pi, true)
+
+
+                    mgr.notify(1337, builder.build())
+                }
+                else {
+                    Intent(context, ReminderActivity::class.java).apply {
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        putExtra(ALARM_ID, id)
+                        context.startActivity(this)
+                    }
+                }
             }
-        }
     }
 }
