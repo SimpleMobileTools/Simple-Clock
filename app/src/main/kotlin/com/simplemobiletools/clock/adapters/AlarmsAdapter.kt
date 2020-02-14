232361d807761905332baef3068cf9691dc00fb2
tibbi <tibor@kaputa.sk>
2020-02-14 19:21:49 +0100

    fix #117, fixing some glitch related to alarms toggline on/off

--- app/src/main/kotlin/com/simplemobiletools/clock/adapters/AlarmsAdapter.kt
+++ app/src/main/kotlin/com/simplemobiletools/clock/adapters/AlarmsAdapter.kt
@@ -100,7 +100,7 @@ class AlarmsAdapter(activity: SimpleActivity, var alarms: ArrayList<Alarm>, val 
 
             alarm_switch.isChecked = alarm.isEnabled
             alarm_switch.setColors(textColor, adjustedPrimaryColor, backgroundColor)
-            alarm_switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            alarm_switch.setOnClickListener {
                 if (alarm.days > 0) {
                     if (activity.config.wasAlarmWarningShown) {
                         toggleAlarmInterface.alarmToggled(alarm.id, alarm_switch.isChecked)
--- app/src/main/kotlin/com/simplemobiletools/clock/fragments/AlarmFragment.kt
+++ app/src/main/kotlin/com/simplemobiletools/clock/fragments/AlarmFragment.kt
@@ -1,10 +1,10 @@
 package com.simplemobiletools.clock.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.simplemobiletools.clock.R
 import com.simplemobiletools.clock.activities.MainActivity
 import com.simplemobiletools.clock.activities.SimpleActivity
