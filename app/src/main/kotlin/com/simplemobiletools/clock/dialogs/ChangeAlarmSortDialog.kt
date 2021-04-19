7ff56ac15ab880b77a06478b26712a55d82e5de7
tibbi <tibor@kaputa.sk>
2021-04-19 15:00:24 +0200

    updating the sorting strings

--- app/src/main/kotlin/com/simplemobiletools/clock/dialogs/ChangeAlarmSortDialog.kt
+++ app/src/main/kotlin/com/simplemobiletools/clock/dialogs/ChangeAlarmSortDialog.kt
@@ -10,29 +10,27 @@ import com.simplemobiletools.commons.extensions.setupDialogStuff
 import kotlinx.android.synthetic.main.dialog_change_alarm_sort.view.*
 
 class ChangeAlarmSortDialog(val activity: BaseSimpleActivity, val callback: (AlarmSort) -> Unit) {
-    private var view: View
+    private var view: View = activity.layoutInflater.inflate(R.layout.dialog_change_alarm_sort, null).apply {
+        val activeRadioButton = when (activity.config.alarmSort) {
+            AlarmSort.CREATED_AT -> sorting_dialog_radio_creation_order
+            AlarmSort.TIME_OF_DAY -> sorting_dialog_radio_alarm_time
+        }
+        activeRadioButton?.isChecked = true
+    }
 
     init {
-        view = activity.layoutInflater.inflate(R.layout.dialog_change_alarm_sort, null).apply {
-            val activeRadioButton = when (activity.config.alarmSort) {
-                AlarmSort.CREATED_AT -> sorting_dialog_radio_created_at
-                AlarmSort.TIME_OF_DAY -> sorting_dialog_radio_time_of_day
-            }
-            activeRadioButton?.isChecked = true
-        }
-
         AlertDialog.Builder(activity)
-                .setPositiveButton(R.string.ok) { _, _ -> dialogConfirmed() }
-                .setNegativeButton(R.string.cancel, null)
-                .create().apply {
-                    activity.setupDialogStuff(view, this, R.string.sort_by)
-                }
+            .setPositiveButton(R.string.ok) { _, _ -> dialogConfirmed() }
+            .setNegativeButton(R.string.cancel, null)
+            .create().apply {
+                activity.setupDialogStuff(view, this, R.string.sort_by)
+            }
     }
 
     private fun dialogConfirmed() {
         val sort = when (view.sorting_dialog_radio_sorting.checkedRadioButtonId) {
-            R.id.sorting_dialog_radio_created_at -> AlarmSort.CREATED_AT
-            R.id.sorting_dialog_radio_time_of_day -> AlarmSort.TIME_OF_DAY
+            R.id.sorting_dialog_radio_creation_order -> AlarmSort.CREATED_AT
+            R.id.sorting_dialog_radio_alarm_time -> AlarmSort.TIME_OF_DAY
             else -> AlarmSort.default()
         }
         activity.config.alarmSort = sort
