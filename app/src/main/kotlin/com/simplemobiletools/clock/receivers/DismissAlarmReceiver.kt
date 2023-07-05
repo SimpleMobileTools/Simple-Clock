728c2b50b19f2fb71a0562495abceba99c9cad6a
Rawlin C <rawlincrasto@gmail.com>
2023-07-05 13:58:24 +0530

    refactor

--- app/src/main/kotlin/com/simplemobiletools/clock/receivers/DismissAlarmReceiver.kt
+++ app/src/main/kotlin/com/simplemobiletools/clock/receivers/DismissAlarmReceiver.kt
@@ -7,6 +7,7 @@ import com.simplemobiletools.clock.extensions.*
 import com.simplemobiletools.clock.helpers.ALARM_ID
 import com.simplemobiletools.clock.helpers.NOTIFICATION_ID
 import com.simplemobiletools.clock.models.Alarm
+import com.simplemobiletools.commons.extensions.removeBit
 import com.simplemobiletools.commons.helpers.ensureBackgroundThread
 import java.util.Calendar
 import kotlin.math.pow
@@ -36,7 +37,7 @@ class DismissAlarmReceiver : BroadcastReceiver() {
     private fun scheduleNextAlarm(alarm: Alarm, context: Context) {
         val oldBitmask = alarm.days
         alarm.days = removeTodayFromBitmask(oldBitmask)
-        context.scheduleNextAlarm(alarm, false)
+        context.scheduleNextAlarm(alarm, true)
         alarm.days = oldBitmask
     }
 
@@ -44,25 +45,8 @@ class DismissAlarmReceiver : BroadcastReceiver() {
         val calendar = Calendar.getInstance()
         calendar.firstDayOfWeek = Calendar.MONDAY
         val dayOfWeek = (calendar.get(Calendar.DAY_OF_WEEK) + 5) % 7
-        // boolean array with each day of the week set as per the bitmask provided
-        val daysOfWeek = BooleanArray(7) { value ->
-            var bitValue = bitmask
-            for (i in 0 until value) {
-                bitValue /= 2
-            }
-            bitValue % 2 == 1
-        }
+        val todayBitmask = 2.0.pow(dayOfWeek).toInt()
 
-        daysOfWeek[dayOfWeek] = false // remove today
-
-        // Convert the boolean array back to an integer (bitmask)
-        var updatedBitmask = 0
-        for (i in 0..6) {
-            if (daysOfWeek[i]) {
-                updatedBitmask += 2.0.pow(i).toInt()
-            }
-        }  // This will return a new bitmask without today included
-
-        return updatedBitmask
+        return bitmask.removeBit(todayBitmask)
     }
 }
